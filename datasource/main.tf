09f1e4adb0d7f32c9612b40d0d04b2635123235f
Adam M <scmlearningcentre@gmail.com>
2020-07-14 05:19:15 +0000

    update

--- datasource/main.tf
+++ datasource/main.tf
@@ -2,11 +2,15 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-data "aws_availability_zones" "example" {
-    state = "available"
-}
+data "aws_instances" "test" {
+  filter {
+    name = "instance.group-id"
+    values = ["sg-9e42dffc"]
+  }
+  filter {
+    name = "instance-type"
+    values = ["t2.micro","t2.small"]
+  }
 
-output "azlist" {
-    value = "${data.aws_availability_zones.example.names}"
+  instance_state_names = ["running", "stopped"]
 }
-
